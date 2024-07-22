0327fb01ca3213d552a7efd49a48351eda9c50de
Ayaan-Jung <202221067@sangmyung.kr>
2024-07-22 18:22:27 +0900

    #4-mypage 임시 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 // import { createStore } from "redux";
 // import rootReducer from "./redux"
-
-import { Provider } from "react-redux"
-import store from "./redux/store"
+import { Provider } from "react-redux";
+import store from "./redux/store";
 // 리덕스에 비동기 작업도 실행시키기 위해 redux-thunk를 적용함
 // const store = createStore(rootReducer)
 
---
+++ src/page/mypage/index.jsx
@@ -0,0 +1,34 @@
+// import React, { useEffect } from "react";
+// import { Outlet, useNavigate } from "react-router-dom";
+
+// const MyPage = () => {
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     navigate("/mypage/intro");
+//   }, []);
+
+//   return <Outlet />;
+// };
+
+// export default MyPage;
+
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import BackButton from "../mypage/components/BackButton";
+
+const MyPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/mypage/intro");
+  }, [navigate]);
+
+  return (
+    <div>
+      <BackButton onClick={() => navigate(-1)} />
+      <Outlet />
+    </div>
+  );
+};
+
+export default MyPage;
