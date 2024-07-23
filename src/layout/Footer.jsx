151365da501bddf531f5e5a1266328b81610410d
kscold <201921339@sangmyung.kr>
2024-07-23 18:39:26 +0900

    #3 - pet 회원가입 뷰 고도화
    pet 회원가입 뷰 고도화, 저장한 펫의 정보를 추가할 수 있는 뷰를 만들었음

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,12 +2,18 @@ import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 
 import { ReactComponent as CommunityLogo } from "../assets/footer/CommunityLogo.svg"
+import { ReactComponent as CommunityLogoActive } from "../assets/footer/CommunityLogoActive.svg"
 
 import { ReactComponent as HomeLogoActive } from "../assets/footer/HomeLogoActive.svg"
 
 import { ReactComponent as RecordLogo } from "../assets/footer/RecordLogo.svg"
-import { ReactComponent as RecordLogoActive } from "../assets/footer/RecordLogoActive.svg"
+// import { ReactComponent as RecordLogoActive } from "../assets/footer/RecordLogo.svg"
+
+import { ReactComponent as MyPage } from "../assets/footer/MyPage.svg"
+import { ReactComponent as MyPageActive } from "../assets/footer/MyPageActive.svg"
+
 import { ReactComponent as RoutineLogo } from "../assets/footer/RoutineLogo.svg"
+import { ReactComponent as RoutineLogoActive } from "../assets/footer/RoutineLogoActive.svg"
 
 const Footer = () => {
   const navigate = useNavigate()
@@ -17,8 +23,18 @@ const Footer = () => {
 
   return (
     <>
+      <div className="footer-item" onClick={() => navigate("/walk")}>
+        {isActive("/walk") ? <HomeLogoActive /> : <HomeLogoActive />}
+        <div
+          className={
+            isActive("/walk") ? "footer-item-title active" : "footer-item-title"
+          }
+        >
+          산책
+        </div>
+      </div>
       <div className="footer-item" onClick={() => navigate("/routine")}>
-        {isActive("/routine") ? <RoutineLogo /> : <RoutineLogo />}
+        {isActive("/routine") ? <RoutineLogoActive /> : <RoutineLogo />}
         <div
           className={
             isActive("/routine")
@@ -30,7 +46,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-item" onClick={() => navigate("/record")}>
-        {isActive("/record") ? <RecordLogoActive /> : <RecordLogo />}
+        {isActive("/record") ? <RecordLogo /> : <RecordLogo />}
 
         <div
           className={
@@ -42,18 +58,9 @@ const Footer = () => {
           기록
         </div>
       </div>
-      <div className="footer-item" onClick={() => navigate("/walk")}>
-        {isActive("/walk") ? <HomeLogoActive /> : <HomeLogoActive />}
-        <div
-          className={
-            isActive("/walk") ? "footer-item-title active" : "footer-item-title"
-          }
-        >
-          산책
-        </div>
-      </div>
+
       <div className="footer-item" onClick={() => navigate("/community")}>
-        {isActive("/community") ? <CommunityLogo /> : <CommunityLogo />}
+        {isActive("/community") ? <CommunityLogoActive /> : <CommunityLogo />}
         <div
           className={
             isActive("/community")
@@ -65,7 +72,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-item" onClick={() => navigate("/mypage")}>
-        {isActive("/mypage") ? <RecordLogoActive /> : <RecordLogo />}
+        {isActive("/mypage") ? <MyPageActive /> : <MyPage />}
         <div
           className={
             isActive("/mypage")
--- src/page/mypage/index.jsx
+++ src/page/mypage/index.jsx
@@ -1,34 +1,7 @@
-// // import React, { useEffect } from "react";
-// // import { Outlet, useNavigate } from "react-router-dom";
+import React from "react"
 
-// // const MyPage = () => {
-// //   const navigate = useNavigate();
-// //   useEffect(() => {
-// //     navigate("/mypage/intro");
-// //   }, []);
+const index = () => {
+  return <div>index</div>
+}
 
-// //   return <Outlet />;
-// // };
-
-// // export default MyPage;
-
-// import React, { useEffect } from "react"
-// import { Outlet, useNavigate } from "react-router-dom"
-// import BackButton from "../auth/register/components/BackButton"
-
-// const MyPage = () => {
-//   const navigate = useNavigate()
-
-//   useEffect(() => {
-//     navigate("/mypage/intro")
-//   }, [navigate])
-
-//   return (
-//     <div>
-//       <BackButton onClick={() => navigate(-1)} />
-//       <Outlet />
-//     </div>
-//   )
-// }
-
-// export default MyPage
+export default index
